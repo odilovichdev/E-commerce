82c6a05abacab563de16f4640b5c1b2137698019
odilovichdev <fazliddinn.gadoyev@gmail.com>
2025-01-27 09:13:10 +0500

    git cache remove

--- store/admin.py
+++ store/admin.py
@@ -8,6 +8,12 @@ class ProductModelAdmin(admin.ModelAdmin):
     # exclude = ("slug",)
     readonly_fields = ("slug",)
 
+    def get_fields(self, request, obj = None):
+        fields = super().get_fields(request, obj)
+        if obj:
+            return fields 
+        return [field for field in fields if field != "slug"]
+
 
 @admin.register(ProductCategory)
 class ProductCategoryModelAdmin(admin.ModelAdmin):
